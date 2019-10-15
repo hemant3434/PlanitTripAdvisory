8776216f018b481fbc35efeafcb5eabe7ce8d451
Hemant Bhanot <hemant.bhanot01@gmail.com>
2019-10-15 17:20:45 -0400

    Update MainController.java

--- backend/src/main/java/krusty_krab/krusty_krab/controller/MainController.java
+++ backend/src/main/java/krusty_krab/krusty_krab/controller/MainController.java
@@ -8,5 +8,20 @@ import org.springframework.http.*;
 @RestController
 @RequestMapping("/api/v1/")
 public class MainController {
+  
+  @GetMapping("/getDummy1")
+  public ResponseEntity<?> getDummy1(@RequestBody Map<String, Object> body) {
+    
+    Map<String, Object> map = new HashMap<String, Object>();
 
+    return ResponseEntity.ok().body(map);
+  }
+  
+  @GetMapping("/getDummy2")
+  public ResponseEntity<?> getDummy2(@RequestBody Map<String, Object> body) {
+    
+    Map<String, Object> map = new HashMap<String, Object>();
+
+    return ResponseEntity.ok().body(map);
+  }
 }
