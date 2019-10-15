5ded51328558060700af0d126af3b628b02c0dfc
Jason <jason.h.7249@gmail.com>
2019-10-15 17:02:59 -0400

    added controller and domain packages

---
+++ backend/src/main/java/krusty_krab/krusty_krab/controller/Event.java
@@ -0,0 +1,27 @@
+package krusty_krab.krusty_krab.controller;
+
+import java.util.List;
+
+public class Event {
+
+	private String name;
+	private String date;
+	private String location;
+	private List<String> activies;
+	private int rating;
+	private double cost;
+	
+	public Event() {
+	}
+	
+	public Event(String name, String date, String location, List<String> activities, int rating, double cost) {
+		this.name = name;
+		this.date = date;
+		this.location = location;
+		this.activies = activities;
+		this.rating = rating;
+		this.cost = cost;
+	}
+	
+	public 
+}
