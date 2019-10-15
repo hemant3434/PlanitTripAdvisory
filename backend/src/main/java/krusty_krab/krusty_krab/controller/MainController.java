73aeee8ed7198f8145b95c9c2cb211ba0334a1db
Hemant Bhanot <hemant.bhanot01@gmail.com>
2019-10-15 17:13:01 -0400

    fixed controller

---
+++ backend/src/main/java/krusty_krab/krusty_krab/controller/MainController.java
@@ -0,0 +1,12 @@
+package krusty_krab.krusty_krab.controller;
+
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class MainController {
+
+}
