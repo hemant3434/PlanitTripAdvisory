a49ea168d8b20b77f18770e33773e1036f1efaee
Richard Hong <r.hong@protonmail.com>
2019-10-16 10:17:34 -0400

    implementing travelCard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,6 @@ export default function App() {
     <View style={styles.container}>
       <Text>PLAN YO SHIT</Text>
       <CardsContainer />
-      <Calendar />
     </View>
   );
 }
--- frontend/src/sections/CardsContainer.js
+++ frontend/src/sections/CardsContainer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View  } from 'react-native';
+import { StyleSheet, View, ScrollView  } from 'react-native';
 import {
   Container,
   Header,
@@ -16,52 +16,24 @@ import {
 } from "native-base";
 import ImageCard from './../components/Cards/ImageCard';
 
-class CardsContainer extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            test: "",
-        }
-
-        fetch("")
-        .then(response => response.json())
-        .then((responseJson) => {
-            this.setState({
-                test: responseJson
-            })
-        })
-    }
-
-    render() {
-        return (
-
-        );
-    }
+export default function CardsContainer() {
+  return (
+      <ScrollView
+      style={StyleSheet.absoluteFill}
+      contentContainerStyle={styles.scrollview}
+      >
+          <ImageCard title="Toronto" image={require("../images/toronto.jpg")} text="lmao"/>
+          <ImageCard title="Scarborough" image={require("../images/toronto.jpg")} text="lmao"/>
+          <ImageCard title="China" image={require("../images/toronto.jpg")} text="420"/>
+      </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });  
-
-export default CardsContainer;
-
-// export default function CardsContainer() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>CARDS GO HERE!</Text>
-//       <Content>
-//         <List>
-//           <ListItem>
-//             <ImageCard title={this.state.test} image={require("../images/toronto.jpg")} text="lmao"/>
-//             <ImageCard title="Scarborough" image={require("../images/toronto.jpg")} text="HAHA"/>
-//           </ListItem>
-//         </List>
-//       </Content>
-//     </View>
-//   );
-// }
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
