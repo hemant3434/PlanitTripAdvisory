074e4f96ffe45781f9305fd40002b5f01a0a0fe7
Swarup Srini <gswarup2001@gmail.com>
2019-10-15 22:23:16 -0400

    Made some mods

--- frontend/src/sections/CardsContainer.js
+++ frontend/src/sections/CardsContainer.js
@@ -16,27 +16,52 @@ import {
 } from "native-base";
 import ImageCard from './../components/Cards/ImageCard';
 
-export default function CardsContainer() {
-  return (
-    <View style={styles.container}>
-      <Text>CARDS GO HERE!</Text>
-      <Content>
-        <List>
-          <ListItem>
-            <ImageCard title="Toronto" image={require("../images/toronto.jpg")} text="lmao"/>
-            <ImageCard title="Scarborough" image={require("../images/toronto.jpg")} text="HAHA"/>
-          </ListItem>
-        </List>
-      </Content>
-    </View>
-  );
+class CardsContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            test: "",
+        }
+
+        fetch("")
+        .then(response => response.json())
+        .then((responseJson) => {
+            this.setState({
+                test: responseJson
+            })
+        })
+    }
+
+    render() {
+        return (
+
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });  
+
+export default CardsContainer;
+
+// export default function CardsContainer() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>CARDS GO HERE!</Text>
+//       <Content>
+//         <List>
+//           <ListItem>
+//             <ImageCard title={this.state.test} image={require("../images/toronto.jpg")} text="lmao"/>
+//             <ImageCard title="Scarborough" image={require("../images/toronto.jpg")} text="HAHA"/>
+//           </ListItem>
+//         </List>
+//       </Content>
+//     </View>
+//   );
+// }
