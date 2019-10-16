3fc5e19b9d2503558591a00efc2c59c947195cf7
Logan Chester <lchester87@gmail.com>
2019-10-16 16:37:39 -0400

    Added date and time container

---
+++ frontend/src/components/TimePicker.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import DateTimePicker from "react-native-modal-datetime-picker";
+
+export default class TimePicker extends Component {
+  state = {
+    isStartTimePickerVisible: false,
+    isEndTimePickerVisible: false,
+    selectedStartTime: "",
+    selectedEndTime: ""
+  };
+
+  showStartTimePicker = () => {
+    this.setState({ isStartTimePickerVisible: true });
+  };
+
+  hideStartTimePicker = () => {
+    this.setState({ isStartTimePickerVisible: false });
+  };
+
+  showEndTimePicker = () => {
+    this.setState({ isEndTimePickerVisible: true });
+  };
+
+  hideEndTimePicker = () => {
+    this.setState({ isEndTimePickerVisible: false });
+  };
+
+  handleStartTimePicked = startTime => {
+    this.setState({ selectedStartTime: startTime.toString() });
+    this.hideStartTimePicker();
+  };
+
+  handleEndTimePicked = endTime => {
+    this.setState({ selectedEndTime: endTime.toString() });
+    this.hideEndTimePicker();
+  };
+
+  render() {
+    const { isStartTimePickerVisible, isEndTimePickerVisible, selectedStartTime, selectedEndTime } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Button title="Start Time" onPress={this.showStartTimePicker} />
+        <Text style={styles.text}>{selectedStartTime}</Text>
+        <DateTimePicker
+          isVisible={isStartTimePickerVisible}
+          onConfirm={this.handleStartTimePicked}
+          onCancel={this.hideStartTimePicker}
+          mode="time"
+        />
+        <Button title="End Time" onPress={this.showEndTimePicker} />
+        <Text style={styles.text}>{selectedEndTime}</Text>
+        <DateTimePicker
+          isVisible={isEndTimePickerVisible}
+          onConfirm={this.handleEndTimePicked}
+          onCancel={this.hideEndTimePicker}
+          mode="time"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  text: {
+    marginVertical: 10
+  }
+});
---
+++ frontend/src/sections/DateTimeContainer.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, View, ScrollView  } from 'react-native';
+import {
+  Container,
+  Header,
+  Title,
+  Content,
+  Button,
+  Icon,
+  List,
+  ListItem,
+  Text,
+  Left,
+  Right,
+  Body
+} from "native-base";
+import Calendar from './../components/Calendar';
+import TimePicker from './../components/TimePicker';
+
+export default function DateTimeContainer() {
+  return (
+      <ScrollView
+      style={StyleSheet.absoluteFill}
+      contentContainerStyle={styles.scrollview}
+      >
+          <Calendar style={styles.header}/>
+          <TimePicker />
+      </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    flex: 1,
+  },
+});
