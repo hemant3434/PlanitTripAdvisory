d9639beb9462bf0b251e96c3f26c7d39c4812091
H-Richard <r.hong@protonmail.com>
2019-10-20 18:14:37 -0400

    tmp

--- frontend/src/Main.js
+++ frontend/src/Main.js
@@ -1,29 +1,101 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import MapPicker from './components/MapPicker';
-import CardsContainer from './sections/CardsContainer';
-import Calendar from './components/Calendar';
-import BottomNav from './components/Navigation/BottomNav';
+
+
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
+import Icon from "react-native-vector-icons/FontAwesome";
 
 
 export default class HybridApp extends React.Component {
-
-  render(){
+  render() {
     return (
-      <View style={styles.container}>
-        <Text>PLAN YO SHIT</Text>
-        <CardsContainer />
-        <BottomNav />
+        <AppContainer />
+    );
+  }
+}
+
+class HomeScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text> This is my Home screen </Text>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+class ExploreScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#d0d0d0'}}>
+        <Text> This is my Explore screen </Text>
+      </View>
+    );
+  }
+}
+
+class NotificationsScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fff'}}>
+        <Text> This is my Notifications screen </Text>
+      </View>
+    );
+  }
+}
+
+class ProfileScreen extends React.Component {
+  render() {
+    return(
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#d0d0d0'}}>
+        <Text> This is my Profile screen </Text>
+      </View>
+    );
+  }
+}
+
+
+const bottomTabNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="home" size={25} color={tintColor} />
+        )
+      }
+    },
+    Explore: {
+      screen: ExploreScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="comments" size={25} color={tintColor} />
+        )
+      }
+    },
+    Notifications: {
+      screen: NotificationsScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="search" size={25} color={tintColor} />
+        )
+      }
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <Icon name="user" size={25} color={tintColor} />
+        )
+      }
+    },
   },
-});
+  {
+    initialRouteName: 'Home',
+    tabBarOptions: {
+      activeTintColor: '#eb6e3d'
+    }
+  }
+);
+
+const AppContainer = createAppContainer(bottomTabNavigator);
---
+++ frontend/src/pages/Itinerary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import CardsContainer from './../sections/CardsContainer';
+import BottomNav from './../components/Navigation/BottomNav';
+
+export default class Itinerary extends React.Component {
+  render(){
+    return(
+        <CardsContainer />
+    );
+  }
+}
