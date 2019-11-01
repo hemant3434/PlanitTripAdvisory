d1a3bdbca89613dba64a5f147bb2f01284ae739c
Hemant Bhanot <hemant.bhanot01@gmail.com>
2019-11-01 18:37:46 -0400

    Update GoogleMaps.java

--- backend/src/main/java/krusty_krab/krusty_krab/domain/GoogleMaps.java
+++ backend/src/main/java/krusty_krab/krusty_krab/domain/GoogleMaps.java
@@ -123,7 +123,7 @@ public class GoogleMaps {
     List<Event> events = new ArrayList();
     //Exclude event if time it takes to travel from location to event + expected length of event exceeds difference between starttime and endtime
     
-    /*double lat = 43.7764;
+    double lat = 43.7764;
     double ltd = -79.2318;
     LatLng cur_loc = new LatLng((double)lat, (double)ltd);
     //NearbySearchRequest all_events = PlacesApi.nearbySearchQuery(KEY, cur_loc);
