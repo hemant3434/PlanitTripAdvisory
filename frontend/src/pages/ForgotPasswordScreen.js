6650797f8e741a78a16275b7439e7ba1874063d6
Swarup Srini <gswarup2001@gmail.com>
2019-11-01 01:59:46 -0400

    created login page and forgot password screen KRUS-58

---
+++ frontend/src/pages/ForgotPasswordScreen.js
@@ -0,0 +1,75 @@
+import React, { memo, useState } from 'react';
+import { Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { emailValidator } from '../services/utils';
+import Background from '../components/common/LoginRegister/Background';
+import BackButton from '../components/common/LoginRegister/BackButton';
+import Logo from '../components/common/LoginRegister/Logo';
+import Header from '../components/common/LoginRegister/Header';
+import TextInput from '../components/common/LoginRegister/TextInput';
+import { theme } from '../services/theme';
+import Button from '../components/common/LoginRegister/Button';
+
+const ForgotPasswordScreen = ({ navigation }) => {
+  const [email, setEmail] = useState({ value: '', error: '' });
+
+  const _onSendPressed = () => {
+    const emailError = emailValidator(email.value);
+
+    if (emailError) {
+      setEmail({ ...email, error: emailError });
+      return;
+    }
+
+    navigation.navigate('LoginScreen');
+  };
+
+  return (
+    <Background>
+      <BackButton goBack={() => navigation.navigate('LoginScreen')} />
+
+      <Logo />
+
+      <Header>Restore Password</Header>
+
+      <TextInput
+        label="E-mail address"
+        returnKeyType="done"
+        value={email.value}
+        onChangeText={text => setEmail({ value: text, error: '' })}
+        error={!!email.error}
+        errorText={email.error}
+        autoCapitalize="none"
+        autoCompleteType="email"
+        textContentType="emailAddress"
+        keyboardType="email-address"
+      />
+
+      <Button mode="contained" onPress={_onSendPressed} style={styles.button}>
+        Send Reset Instructions
+      </Button>
+
+      <TouchableOpacity
+        style={styles.back}
+        onPress={() => navigation.navigate('LoginScreen')}
+      >
+        <Text style={styles.label}>← Back to login</Text>
+      </TouchableOpacity>
+    </Background>
+  );
+};
+
+const styles = StyleSheet.create({
+  back: {
+    width: '100%',
+    marginTop: 12,
+  },
+  button: {
+    marginTop: 12,
+  },
+  label: {
+    color: theme.colors.secondary,
+    width: '100%',
+  },
+});
+
+export default memo(ForgotPasswordScreen);
