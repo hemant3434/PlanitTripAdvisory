1a7fd1073dea37d5c2c234afd5e0a55efc5e14f5
H-Richard <r.hong@protonmail.com>
2019-11-12 18:45:03 -0500

    removing unused imports

--- frontend/src/pages/MapPage.js
+++ frontend/src/pages/MapPage.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import MapPicker from '../components/common/MapPicker/MapPicker';
 
-
-
 export default class ExplorePage extends React.Component {
   render(){
     return(
