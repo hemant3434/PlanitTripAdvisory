5fcfcfab44b4c7addcc8f2ed0d40ee8054e605da
H-Richard <r.hong@protonmail.com>
2019-11-12 18:42:38 -0500

    working on universal styling

---
+++ frontend/src/styles/styles.js
@@ -0,0 +1,10 @@
+import { Stylesheet } from 'react-native';
+
+const styles = Stylesheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center'
+    }
+})
+
+export default styles;
